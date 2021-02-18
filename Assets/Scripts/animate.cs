88c21094025d7eed6f04fe35fc6c755bc324f035
Area-Turtle <60633052+Area-Turtle@users.noreply.github.com>
2021-02-18 00:14:35 -0800

    add scene change

---
+++ Assets/Scripts/animate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate: MonoBehaviour
+{
+    private Rigidbody rb;
+    private float jumpSpeed;
+    private float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void jumpButton()
+    {
+        rb.velocity = Vector2.up * jumpSpeed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
