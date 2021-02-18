b4cd9383cc8137c82295d95bce129b6aa360f22f
Area-Turtle <60633052+Area-Turtle@users.noreply.github.com>
2021-02-18 11:09:10 -0800

    add day/night

--- Assets/Scripts/animate.cs
+++ Assets/Scripts/animate.cs
@@ -2,24 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Animate: MonoBehaviour
 {
-    private Rigidbody rb;
-    private float jumpSpeed;
-    private float speed = 5;
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-    }
 
-    public void jumpButton()
-    {
-        rb.velocity = Vector2.up * jumpSpeed;
     }
-    // Update is called once per frame
+    
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Animator>().Play("CubeMove");
+        }
         
     }
+    
 }
